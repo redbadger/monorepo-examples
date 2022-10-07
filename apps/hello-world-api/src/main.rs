8127c4689eb8ab1349f59c34f8781356cdf58a8e
Jenny Sharps <jenny.sharps@red-badger.com>
2022-10-07 16:57:21 +0100

    fix Docker image for rust lib

--- apps/hello-world-api/src/main.rs
+++ apps/hello-world-api/src/main.rs
@@ -18,7 +18,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .route("/", web::get().to(greet))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
